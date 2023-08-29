b6524d4e9efbf477f40a13ebe6f1fd08434cf388
ahad1033 <a.ahad2232@gmail.com>
2023-08-30 01:30:02 +0600

    featured section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Banner from './components/Banner/Banner'
+import FeaturedProducts from './components/FeaturedProducts/FeaturedProducts'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='max-w-screen-lg mx-auto'>
       <Navbar />
       <Banner />
+      <FeaturedProducts />
     </div>
   )
 }
